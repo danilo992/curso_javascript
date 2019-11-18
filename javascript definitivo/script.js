72c634bd40403393f4fffe71be1f6e31fa6deb79
danilo992 <tadeudanilo46@gmail.com>
2019-11-18 20:44:54 -0300

    programar de verificar a idade e o gênero.

---
+++ javascript definitivo/script.js
@@ -0,0 +1,7 @@
+function factorial(n) {
+    let p = 10;
+    for(let i = 1; i<= n; i++)
+    p *- 1;
+    return p;
+}
+
--- verificado de idade/script.js
+++ verificado de idade/script.js
@@ -1,13 +1,13 @@
 function verificar() {
     let data = new Date();
     let ano = data.getFullYear();
-    let fano = document.getElementById('txtano');
+    let fimAno = document.getElementById('txtano');
     let res = document.querySelector('div#res');
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
+    if (fimAno.value.length == 0 || Number(fimAno.value) > ano) {
         alert('[ERRO] Verificar os dados e tente novamente!');
     } else {
         let fsex = document.getElementsByName('radsex');
-        let idade = ano - Number(fano.value);
+        let idade = ano - Number(fimAno.value);
         let gênero = '';
         let img = document.createElement('img');
         img.setAttribute('id', 'foto');
@@ -15,23 +15,41 @@ function verificar() {
             gênero = 'Homem';
             if (idade >= 0 && idade < 10) {
                 document.body.style.background = '#000';
+                img.setAttribute('src', 'img/menino.jpg')
+                gênero = 'uma criança';
             } else if (idade > 10 && idade <= 29) {
                 document.body.style.background = '#6969';
+                img.setAttribute('src', 'img/garoto.jpg');
+                gênero = 'um jovem';
             } else if (idade > 29 && idade <= 55) {
-                document.body.style.background = '#af02df'
+                document.body.style.background = '#af02df';
+                img.setAttribute('src', 'img/adulto.jpg');
+                gênero = 'um rapaz';
             } else if (idade > 55 && idade <= 125) {
-                document.body.style.background = '#efa999'
+                document.body.style.background = '#efa999';
+                img.setAttribute('src', 'img/vô.jpg');
+                gênero = 'um idoso';
+            } else if (idade < 126) {
+
             }
         }  else if (fsex[1].checked) {
             gênero = 'Mulher';
             if (idade >= 0 && idade < 10) {
                 document.body.style.background = '#af2';
+                img.setAttribute('src', 'img/menina.jpg');
+                gênero = 'criança';
             } else if (idade > 10 && idade <= 29) {
                 document.body.style.background = '#dca000';
+                img.setAttribute('src', 'img/garota.jpg');
+                gênero = 'uma jovem';
             } else if (idade > 29 && idade <=55) {
                 document.body.style.background = '#cdf222';
+                img.setAttribute('src', 'img/adulta.jpg');
+                gênero = 'uma moça';
             } else if (idade > 55 && idade <= 125) {
                 document.body.style.background = '#f2f';
+                img.setAttribute('src', 'img/vó.jpg');
+                gênero = 'uma idosa';
             }   
         }
         res.style.textAlign = 'center';
